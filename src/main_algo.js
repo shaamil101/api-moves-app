56afcaf4488a3e142da1182b45f2294c096bd6b9
harryirwin <henry.r.irwin.25@dartmouth.edu>
2024-05-28 10:38:37 -0400

    inital commit

--- src/main_algo.js
+++ src/main_algo.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
+
 dotenv.config({ silent: true });
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const maxRequests = 5;
+const maxRequests = 1;
 let requestCounter = 0;
 
 function fetchAllPlaces(url, places = []) {
@@ -29,10 +30,15 @@ function fetchAllPlaces(url, places = []) {
     });
 }
 
+function getPhotoUrl(photoReference, maxWidth = 400) {
+  const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=${maxWidth}&photoreference=${photoReference}&key=${process.env.PLACES_API_KEY}`;
+  return url;
+};
+
 async function callGPT(prompt) {
   try {
     const response = await openai.chat.completions.create({
-      model: 'gpt-4o',
+      model: 'gpt-4',
       messages: [{ role: 'user', content: prompt }],
     });
 
@@ -50,53 +56,65 @@ export default async function getResultJson(results, location, radius) {
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationString}&radius=${radius}&type=${typeRes}|${typeBar}|${typeClub}&key=${process.env.PLACES_API_KEY}`;
 
   const places = await fetchAllPlaces(url);
-  const placesTrunc = places.map((place) => {
-    return {
-      name: place.name,
-      price_level: place.price_level,
-      rating: place.rating,
-      types: place.types,
-    };
-  });
-  const prompt = `Select the twenty best options from the following places and return their names as a simple array, just that as output please ${JSON.stringify(placesTrunc)} you should select the places according to the following responses of this survey ${JSON.stringify(results)} as we will later create an itenary for a night out based on these places`;
+  // const placesTrunc = places.map((place) => {
+  //   return {
+  //     name: place.name,
+  //     price_level: place.price_level,
+  //     rating: place.rating,
+  //     types: place.types,
+  //   };
+  // });
+  // const prompt = `Select the twenty best options from the following places and return their names as a simple array, just that as output please ${JSON.stringify(placesTrunc)} you should select the places according to the following responses of this survey ${JSON.stringify(results)} as we will later create an itenary for a night out based on these places`;
+  // const options = await callGPT(prompt);
+  // const optionsArray = JSON.parse(options.match(/\[(.*?)\]/s)[0]);
+  // const optionsJson = places.filter((place) => { return optionsArray.includes(place.name); }).map((place) => {
+  //   return {
+  //     name: place.name,
+  //     photos: place.photos,
+  //     place_id: place.place_id,
+  //     price_level: place.price_level,
+  //     rating: place.rating,
+  //     types: place.types,
+  //     user_ratings_total: place.user_ratings_total,
+  //   };
+  // });
 
-  const options = await callGPT(prompt);
-  const optionsArray = JSON.parse(options.match(/\[(.*?)\]/s)[0]);
-  const optionsJson = places.filter((place) => { return optionsArray.includes(place.name); }).map((place) => {
+
+  // for (let i = 0; i < optionsJson.length; i += 1) {
+  //   const placeId = optionsJson[i].place_id;
+  //   const revUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${process.env.PLACES_API_KEY}`;
+  //   // eslint-disable-next-line no-await-in-loop
+  //   const response = await axios.get(revUrl);
+  //   const reviews = response.data.result.reviews.slice(0, 3);
+  //   const reviewTexts = reviews.map((review) => { return review.text; });
+  //   optionsJson[i].reviews = reviewTexts;
+  // }
+
+  // const finalPrompt = `select an itenerary for a night out from the following places ${JSON.stringify(optionsJson)} based on the following responses of this survey ${JSON.stringify(results)} please return just the json of the places in the order in which they should be visited you should only include as many places as would make sense to go to on one night out based on the survey results. please return raw text with no code block ticks`;
+  // const finalRes = await callGPT(finalPrompt);
+  // const finalResult = JSON.parse(finalRes).map((place) => {
+  //   return {
+  //     name: place.name,
+  //     photos: place.photos,
+  //     place_id: place.place_id,
+  //     rating: place.rating,
+  //   };
+  // });
+
+  // return finalResult;
+
+  console.log(places);
+
+  const finalPlaces = places.map((place) => {
     return {
       name: place.name,
-      photos: place.photos,
+      photo: getPhotoUrl(place.photos[0].photo_reference),
       place_id: place.place_id,
+      rating: place.rating,
       price_level: place.price_level,
-      rating: place.rating,
-      types: place.types,
-      user_ratings_total: place.user_ratings_total,
     };
   });
 
-  for (let i = 0; i < optionsJson.length; i += 1) {
-    const placeId = optionsJson[i].place_id;
-    const revUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${process.env.PLACES_API_KEY}`;
-    // eslint-disable-next-line no-await-in-loop
-    const response = await axios.get(revUrl);
-    const reviews = response.data.result.reviews.slice(0, 3);
-    const reviewTexts = reviews.map((review) => { return review.text; });
-    optionsJson[i].reviews = reviewTexts;
-  }
-
-  const finalPrompt = `select an itenerary for a night out from the following places ${JSON.stringify(optionsJson)} based on the following responses of this survey ${JSON.stringify(results)} please return just the json of the places in the order in which they should be visited you should only include as many places as would make sense to go to on one night out based on the survey results. please return raw text with no code block ticks`;
-  const finalRes = await callGPT(finalPrompt);
-  const finalResult = JSON.parse(finalRes).map((place) => {
-    return {
-      name: place.name,
-      photos: place.photos,
-      place_id: place.place_id,
-      rating: place.rating,
-      user_ratings_total: place.user_ratings_total,
-    };
-  });
-
-  console.log(finalResult);
-
-  return finalResult;
+  const retVal = finalPlaces.slice(0, 3);
+  return retVal;
 }
